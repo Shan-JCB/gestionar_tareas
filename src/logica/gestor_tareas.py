bc0bef85be61fa5b0939dca6db72d9842e9bc535
Shan-JCB <71997263@continental.edu.pe>
2024-06-05 03:37:34 -0500

    Lanzamiento 1.0.0 correcto

--- src/logica/gestor_tareas.py
+++ src/logica/gestor_tareas.py
@@ -7,7 +7,6 @@ class GestorTareas:
     def __init__(self):
         self.tareas = []
 
-
     def agregar_tarea(self, titulo, descripcion):
         if not titulo:
             raise ValueError("El título no puede estar vacío")
